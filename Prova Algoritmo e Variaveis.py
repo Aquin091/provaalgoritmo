2c02780a8faa306d48eadc269c906adce0e666d3
Aquin091 <150545383+Aquin091@users.noreply.github.com>
2023-11-11 15:53:14 -0300

    Add files via upload

---
+++ Prova Algoritmo e Variaveis.py
@@ -0,0 +1,13 @@
+nome = str(input("Qual seu nome?"))
+print(f"Seja bem vindo {nome}")
+
+idade = int(input("Qual sua idade?"))
+print(f"Você tem {idade} anos")
+
+peso = float(input("Qual seu peso?"))
+print(f"Ok, você tem {peso} Kilos")
+
+Teste = 20==15
+print(bool(Teste))
+Teste2 = 15>=15
+print(bool(Teste2))
